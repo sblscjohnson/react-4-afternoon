e3421de2ff082a1ef2e879d494d426802b49bd26
Spence Johnson <spencejohnson@Spences-MacBook-Air.local>
2019-01-22 15:27:49 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-
+import routes from './routes'
+import {HashRouter, Link} from 'react-router-dom';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
-          </div>
-        </nav>
-      </div>
+      <HashRouter>
+        <div>
+          <nav className='nav'>
+            <div>LitFam University</div> 
+            <div className='link-wrap'>
+                <Link className='links' to='/'>Home</Link>
+                <Link className='links' to='/about'>About</Link> 
+            </div>
+          </nav>
+          {routes}
+        </div>
+      </HashRouter>
     )
   }
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ export default class Contact extends Component {
   render() {
     return (
       <div>
-        <h1 className='title'>Contact Information of WestSide University:</h1>
+        <h1 className='title'>Contact Information of LitFam University:</h1>
         <div className='sub_box'>
           <p><strong>Address:</strong> 123 West 4590 North, Paradise UT</p>
           <p><strong>Number:</strong> 1-800-234-5678</p>
